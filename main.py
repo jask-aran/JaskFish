f350b6f4a436545bc3daccd81c47029f87e56b0a
Jaskaran Singh <jaskarann.singhh@gmail.com>
2024-06-25 16:21:14 +1000

    .

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-fen', help='Set the initial board state to the given FEN string')
+    parser.add_argument('-dev', action='store_true', help='Enable debug mode')
     return parser.parse_args()
     # k7/2Q4P/8/8/8/8/8/K2R4
 
 def main():
     args = parse_args()
+    board = chess.Board() if not args.fen else chess.Board(args.fen)
+    dev = not args.dev # CHANGE LATER
 
     app = QApplication(sys.argv)
-    board = chess.Board() if not args.fen else chess.Board(args.fen)
-    gui = ChessGUI(board, dev=True)
+    gui = ChessGUI(board, dev=dev)
     
     gui.show()
     sys.exit(app.exec_())
---
+++ utils.py
@@ -0,0 +1,23 @@
+from PySide2.QtWidgets import QApplication
+def color_text(text, color_code):
+    return f"\033[{color_code}m{text}\033[0m"
+
+def debug_text(text):
+    return f"{color_text('DEBUG', '31')} {text}"
+
+
+
+def get_piece_unicode(piece):
+    piece_unicode = {
+        'K': '♔', 'Q': '♕', 'R': '♖', 'B': '♗', 'N': '♘', 'P': '♙',
+        'k': '♚', 'q': '♛', 'r': '♜', 'b': '♝', 'n': '♞', 'p': '♟'
+    }
+    return piece_unicode[piece.symbol()]
+
+def center_on_screen(window):
+    screen = QApplication.primaryScreen()
+    screen_geometry = screen.geometry()
+    window_size = window.size()
+    x = (screen_geometry.width() - window_size.width()) / 2 + screen_geometry.left()
+    y = (screen_geometry.height() - window_size.height()) / 2 + screen_geometry.top()
+    window.move(x, y)
